d1df9ffdc1b2bf0aee5326a77964564d896d21a1
Loren <loren.boshkoski@sorsix.com>
2024-05-08 14:42:46 +0200

    front end fix for selecting multiple categories

--- frontend/src/app/components/search-events/search-events.component.ts
+++ frontend/src/app/components/search-events/search-events.component.ts
@@ -3,14 +3,14 @@ import { EventService } from '../../services/event.service';
 import { BehaviorSubject, Subject, debounceTime, distinctUntilChanged, switchMap, tap } from 'rxjs';
 import { EventInterface } from '../../interfaces/event';
 import { EventComponent } from '../event/event.component';
-import {MatChipsModule} from '@angular/material/chips';
+import { MatChipsModule } from '@angular/material/chips';
 import { FormsModule } from '@angular/forms';
 import { MatIconModule } from '@angular/material/icon';
-import {MatInputModule} from '@angular/material/input'
-import {MatDatepickerModule} from '@angular/material/datepicker';
-import {MatSelectModule} from '@angular/material/select';
+import { MatInputModule } from '@angular/material/input'
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatSelectModule } from '@angular/material/select';
 
-import {} from 'mdb-angular-ui-kit/'
+import { } from 'mdb-angular-ui-kit/'
 import { NgFor } from '@angular/common';
 import { provideNativeDateAdapter } from '@angular/material/core';
 import { Router, RouterLink, RouterOutlet } from '@angular/router';
@@ -35,89 +35,108 @@ import { Router, RouterLink, RouterOutlet } from '@angular/router';
 export class SearchEventsComponent {
 
   cities: any[] = [
-    {value: 'skopje', viewValue: 'Skopje'},
-    {value: 'tetovo', viewValue: 'Tetovo'},
-    {value: 'veles', viewValue: 'Veles'},
+    { value: 'skopje', viewValue: 'Skopje' },
+    { value: 'tetovo', viewValue: 'Tetovo' },
+    { value: 'veles', viewValue: 'Veles' },
   ];
 
   events: EventInterface[] | undefined;
   recentEvents: EventInterface[] | undefined;
   allEvents: EventInterface[] | undefined;
+
   categories: string[] | undefined;
+  selectedCategories: string[] = [];
 
   query$: Subject<string> = new Subject();
+
+  //ako ne filtrirame od backend za kategorii moze da se izbrise ova
   category$ = new BehaviorSubject<string | null>(null);
 
   loading: boolean = true;
 
-  clicked: boolean = false;
+  //clicked: boolean = false;
 
-  constructor(private eventService: EventService){}
+  constructor(private eventService: EventService) { }
 
   ngOnInit(): void {
 
     this.eventService.getEvents()
-    .subscribe(
-      events => {
-        this.events = events
-        this.allEvents = events
-        this.loading = false
-      }
-    );
+      .subscribe(
+        events => {
+          this.events = events
+          this.allEvents = events
+          this.loading = false
+        }
+      );
 
     this.eventService.getRecentEvents()
-    .subscribe(
-      recentEvents => {
-        this.recentEvents = recentEvents
-        this.loading = false
-      }
-    );
+      .subscribe(
+        recentEvents => {
+          this.recentEvents = recentEvents
+          this.loading = false
+        }
+      );
 
     this.eventService.getEventCategories()
-    .subscribe(
-      categories =>{
+      .subscribe(
+        categories => {
           this.categories = categories
           console.log(categories)
-      }
-    )
+        }
+      )
 
     this.query$.pipe(
       debounceTime(400),
       distinctUntilChanged(),
       switchMap(query => this.eventService.search(query))
-      ).subscribe(
-        result => {
-          this.events = result
-          this.loading = false
-        }
+    ).subscribe(
+      result => {
+        this.events = result
+        this.loading = false
+      }
     );
 
-    this.category$.pipe(
-      debounceTime(400),
-      distinctUntilChanged(),
-      switchMap(category => this.eventService.filterByCategory(category!!))
-      ).subscribe(
-        result => {
-          this.events = result
-          this.loading = false
-          this.clicked = true
-        }
-    );
+    // this.category$.pipe(
+    //   debounceTime(400),
+    //   distinctUntilChanged(),
+    //   switchMap(category => this.eventService.filterByCategory(category!!))
+    //   ).subscribe(
+    //     result => {
+    //       this.events = result
+    //       this.loading = false
+    //       //this.clicked = true
+    //     }
+    // );
 
   }
 
-  search(query: string){
+  search(query: string) {
     this.query$.next(query);
   }
 
-  filterByCategory(category: string){
-    console.log(this.clicked)
-    if(this.category$.value === category && this.clicked===true){
-       this.clicked = false
-       this.events = this.allEvents
+  // filterByCategory(category: string){
+  //   if(this.category$.value === category){
+  //     // this.clicked = false
+  //      this.events = this.allEvents
+  //   }
+  //   else {
+  //     this.category$.next(category)
+  //   }
+  // }
+  filterByCategory(category: string) {
+    if (this.selectedCategories.includes(category)) {
+      this.selectedCategories = this.selectedCategories.filter(c => c !== category);
+    } else {
+      this.selectedCategories.push(category);
+    }
+    
+    if (this.selectedCategories.length === 0) {
+      this.events = this.allEvents
     }
     else {
-      this.category$.next(category)
+      this.events = this.allEvents?.filter(event =>
+        this.selectedCategories.includes(event.category)
+      )
     }
   }
 
