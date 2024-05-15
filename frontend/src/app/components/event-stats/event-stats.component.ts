38668103d486b2f8885c6f034e4c79927a92475e
Andrej Vlahovski <vlahovskiandrej@yahoo.com>
2024-05-15 18:20:12 +0200

    stats page added

--- frontend/src/app/components/event-stats/event-stats.component.ts
+++ frontend/src/app/components/event-stats/event-stats.component.ts
@@ -3,6 +3,9 @@ import { MatButtonModule } from "@angular/material/button";
 import { MatCardModule } from "@angular/material/card";
 import { Chart } from 'chart.js';
 import {MatIconModule} from '@angular/material/icon';
+import { NgxEchartsDirective, provideEcharts } from 'ngx-echarts';
+import { CommonModule } from "@angular/common";
+import { EChartsOption } from 'echarts';
 
 
 @Component({
@@ -11,29 +14,159 @@ import {MatIconModule} from '@angular/material/icon';
   imports: [
     MatCardModule, 
     MatButtonModule,
-    MatIconModule
+    MatIconModule,
+    CommonModule, NgxEchartsDirective
   ],
   templateUrl: './event-stats.component.html',
-  styleUrl: './event-stats.component.css'
+  styleUrl: './event-stats.component.css',
+  providers: [
+    provideEcharts(),
+  ]
 })
-export class EventStatsComponent implements OnInit{
+export class EventStatsComponent{
 
-  ngOnInit(): void {
-    const xValues = ["Italy", "France", "Spain", "USA", "Argentina"];
-    const yValues = [55, 49, 44, 24, 15];
-    const barColors = ["red", "green","blue","orange","brown"];
-
-    new Chart("myChart", {
-      type: "bar",
-      data: {
-        labels: xValues,
-        datasets: [{
-          backgroundColor: barColors,
-          data: yValues
-        }]
-      }
-    });
-  }
+  chartOption: EChartsOption = {
+    title: {
+      text: 'Revenue by month',
+      subtext: 'Real time data',
+      left: 'center'
+    },
+    tooltip: {
+      trigger: 'item'
+    },
+    xAxis: {
+      type: 'category',
+      data: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
+    },
+    yAxis: {
+      type: 'value',
+    },
+    series: [
+      {
+        data: [820, 932, 901, 934, 1290, 1330, 1320],
+        type: 'bar',
+        emphasis: {
+          itemStyle: {
+            shadowBlur: 10,
+            shadowOffsetX: 0,
+            shadowColor: 'rgba(0, 0, 0, 0.5)'
+          }
+        }
+      },
+    ],
+  };
   
-}
 
+  option: EChartsOption = {
+    title: {
+      text: 'Customer split by gender',
+      subtext: 'Real time data',
+      left: 'center'
+    },
+    tooltip: {
+      trigger: 'item'
+    },
+    legend: {
+      orient: 'vertical',
+      left: 'left'
+    },
+    series: [
+      {
+        name: 'Access From',
+        type: 'pie',
+        radius: '50%',
+        data: [
+          { value: 1048, name: 'Man' },
+          { value: 735, name: 'Woman' }
+        ],
+        emphasis: {
+          itemStyle: {
+            shadowBlur: 10,
+            shadowOffsetX: 0,
+            shadowColor: 'rgba(0, 0, 0, 0.5)'
+          }
+        }
+      }
+    ]
+  };
+
+
+  option2: EChartsOption = {
+    title: {
+      text: 'Customer split by age range',
+      subtext: 'Real time data',
+      left: 'center'
+    },
+    tooltip: {
+      trigger: 'item'
+    },
+    legend: {
+      orient: 'vertical',
+      left: 'left'
+    },
+    series: [
+      {
+        name: 'Access From',
+        type: 'pie',
+        radius: '50%',
+        data: [
+          { value: 40, name: '<18' },
+          { value: 38, name: '18..30' },
+          { value: 32, name: '30..40' },
+          { value: 30, name: '40..50' },
+          { value: 28, name: '50..60' },
+          { value: 26, name: '60..70' },
+          { value: 22, name: '70+' },
+        ],
+        emphasis: {
+          itemStyle: {
+            shadowBlur: 10,
+            shadowOffsetX: 0,
+            shadowColor: 'rgba(0, 0, 0, 0.5)'
+          }
+        }
+      }
+    ]
+  };
+
+
+  option3: EChartsOption = {
+    title: {
+      text: 'Ticket sales'
+    },
+    tooltip: {
+      trigger: 'axis',
+      axisPointer: {
+        type: 'shadow'
+      }
+    },
+    legend: {},
+    grid: {
+      left: '3%',
+      right: '4%',
+      bottom: '3%',
+      containLabel: true
+    },
+    xAxis: {
+      type: 'value',
+      boundaryGap: [0, 0.01]
+    },
+    yAxis: {
+      type: 'category',
+      data: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
+    },
+    series: [
+      {
+        name: 'With Coupon',
+        type: 'bar',
+        data: [18203, 23489, 29034, 104970, 131744, 630230, 352541]
+      },
+      {
+        name: 'Without Coupon',
+        type: 'bar',
+        data: [19325, 23438, 31000, 121594, 134141, 681807, 480152]
+      }
+    ]
+  };
+
+}
