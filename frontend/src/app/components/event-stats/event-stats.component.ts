be78a2a193e2ad3b6b982abd32ff6f3fee5fb77e
Andrej Vlahovski <vlahovskiandrej@yahoo.com>
2024-05-15 14:49:24 +0200

    stats added

--- frontend/src/app/components/event-stats/event-stats.component.ts
+++ frontend/src/app/components/event-stats/event-stats.component.ts
@@ -1,13 +1,39 @@
-import { AfterViewInit, Component, ElementRef, ViewChild } from "@angular/core";
+import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from "@angular/core";
+import { MatButtonModule } from "@angular/material/button";
+import { MatCardModule } from "@angular/material/card";
 import { Chart } from 'chart.js';
+import {MatIconModule} from '@angular/material/icon';
+
 
 @Component({
   selector: 'app-event-stats',
   standalone: true,
-  imports: [],
+  imports: [
+    MatCardModule, 
+    MatButtonModule,
+    MatIconModule
+  ],
   templateUrl: './event-stats.component.html',
   styleUrl: './event-stats.component.css'
 })
-export class EventStatsComponent {
+export class EventStatsComponent implements OnInit{
+
+  ngOnInit(): void {
+    const xValues = ["Italy", "France", "Spain", "USA", "Argentina"];
+    const yValues = [55, 49, 44, 24, 15];
+    const barColors = ["red", "green","blue","orange","brown"];
+
+    new Chart("myChart", {
+      type: "bar",
+      data: {
+        labels: xValues,
+        datasets: [{
+          backgroundColor: barColors,
+          data: yValues
+        }]
+      }
+    });
+  }
+  
 }
 
